695a0f7581fdb0e946e95f1d535025f73c35e7ae
luqman <putraian2005@gmail.com>
2022-08-23 11:55:37 +0700

    add new project

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-redis/redis/v7"
+	"github.com/gorilla/mux"
+)
+
+var rdb *redis.Client
+
+func main() {
+
+	rdb = redis.NewClient(&redis.Options{Addr: "localhost:6379"})
+
+	r := mux.NewRouter()
+
+	r.Path("/recipe").Methods("POST").HandlerFunc(createHandler)
+	r.Path("/recipe/{id}").Methods("PUT").HandlerFunc(updateHandler)
+	r.Path("/recipe/{id}").Methods("GET").HandlerFunc(getHandler)
+	r.Path("/recipes").Methods("GET").HandlerFunc(listHandler)
+
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
